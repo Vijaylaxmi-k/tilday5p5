a589d6b1ca3d1f45beff3ef0a8ad06725dade5a8
vijaylaxmi-k <vvkulkarni1992@gmail.com>
2021-07-27 17:35:06 +0530

    Day3Project4

---
+++ daywise/Day3Project4/src/package1/MyClass.java
@@ -0,0 +1,30 @@
+// pgm for abs class using interface and implement
+package package1;
+
+interface Shape
+{ 
+	 int area();
+	 int perimeter();
+	
+}
+
+class Circle implements Shape
+{
+
+	@Override
+	public int area() 
+	{
+		return 0;
+	}
+
+	@Override
+	public int perimeter() 
+	{
+		return 0;
+	}
+	
+}
+		
+public class MyClass {
+
+}
